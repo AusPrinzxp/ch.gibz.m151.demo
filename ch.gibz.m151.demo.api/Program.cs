292eec3e1ee1ffe8b9132117fb28bc2683c7211b
seugster <seugster@online.gibz.ch>
2020-06-15 09:41:09 +0200

    Authentication and Swagger

--- ch.gibz.m151.demo.api/Program.cs
+++ ch.gibz.m151.demo.api/Program.cs
@@ -17,10 +17,11 @@ namespace ch.gibz.m151.demo.api
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+           Host.CreateDefaultBuilder(args)
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.UseStartup<Startup>()
+                       .UseUrls("http://localhost:4000");
+               });
     }
 }
---
+++ ch.gibz.m151.demo.data/DTOs/TodoItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch.gibz.m151.demo.data.DTOs
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
